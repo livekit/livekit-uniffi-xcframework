e3e6a40a72ea043749d40215b63ec7a379f32979
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-12-12 11:53:56 +0100

    Test release (#1)

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "LiveKitUniFFI",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .macCatalyst(.v14),
+        .visionOS(.v2),
+        .tvOS(.v17),
+    ],
+    products: [
+        .library(
+            name: "LiveKitUniFFI",
+            targets: ["LiveKitUniFFI"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "LiveKitUniFFI",
+            dependencies: ["RustLiveKitUniFFI"]
+        ),
+        .binaryTarget(
+            name: "RustLiveKitUniFFI",
+            url: "https://github.com/livekit/livekit-uniffi-xcframework/releases/download/0.0.1/RustLiveKitUniFFI.xcframework.zip",
+            checksum: "982f5e0d965bcbf9840cd50a979bf12e3f4ed74ba4326a15cc4e1aaea1670287"
+        )
+    ]
+)
